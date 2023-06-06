68bd0380fb34c6b96423a9b3d80e198b1debd7c0
taktak-0319 <tka150319@icloud.com>
2023-06-07 00:31:58 +0900

    section

--- section/section5/section5.js
+++ section/section5/section5.js
@@ -1,3 +1,15 @@
-const score = null;
+const score = (x) => {
+    if (80 <= x && x <= 100) {
+        return "合格";
+    } else if (60 <= x && x < 80) {
+        return "追試";
+    } else if (0 <= x && x < 60) {
+        return "不合格";
+    } else if (x === undefined) {
+        return "後日受験";
+    } else {
+        return "0以上100未満の数字を入れてください";
+    }
+};
 
 module.exports = { score };
