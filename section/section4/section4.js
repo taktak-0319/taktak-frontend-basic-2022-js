b2112a12ce06b9beae916dfdfa6eddebbc42b096
taktak-0319 <tka150319@icloud.com>
2023-06-06 17:33:59 +0900

    section4

--- section/section4/section4.js
+++ section/section4/section4.js
@@ -1,5 +1,12 @@
-const echoDiv = null;
+const echoDiv = (x, y) => {
+    return "商: " + ((x / y) | 0) + ", 余り: " + (x % y);
+};
 
-const reply = null;
+const reply = (x) => {
+    if (x === undefined) {
+        return "返信なし";
+    }
+    return x;
+};
 
 module.exports = { echoDiv, reply };
