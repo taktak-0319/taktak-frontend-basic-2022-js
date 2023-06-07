981d4b9312f66c9fbf7c31e04a3aa25a76646597
taktak-0319 <tka150319@icloud.com>
2023-06-08 00:51:36 +0900

    section6

--- section/section6/section6.js
+++ section/section6/section6.js
@@ -1,3 +1,14 @@
-const sum = null;
-
+const sum = (x, y) => {
+    total = 0;
+    if (x >= y) {
+        for (i = 0; x >= y; y++) {
+            total += y;
+        }
+    } else if (x < y) {
+        for (i = 0; x <= y; x++) {
+            total += x;
+        }
+    }
+    return total;
+};
 module.exports = { sum };
