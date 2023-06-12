aaf843955eaa8adecc1b1b75ba12f520ac787970
taktak-0319 <tka150319@icloud.com>
2023-06-12 23:04:55 +0900

    section9

--- section/section9/section9.js
+++ section/section9/section9.js
@@ -1,5 +1,41 @@
-const userIdList = null;
+const userIdList = (datas) => {
+    const returnUserId = [];
+    const allUserIds = datas.map((data) => {
+        return data.userId;
+    });
+    for (userId of allUserIds) {
+        const getUserId = returnUserId.findIndex((eachReturnUserId) => {
+            return eachReturnUserId === userId;
+        });
 
-const fixData = null;
+        if (getUserId === -1) {
+            returnUserId.push(userId);
+        }
+    }
+    return returnUserId;
+};
+
+const fixData = (jsonDates) => {
+    const userIds = userIdList(jsonDates);
+    const array = [];
+    // return userIds;
+    for (userId of userIds) {
+        const machedUserData = jsonDates.filter(
+            (jsondata) => jsondata.userId === userId
+        );
+        for (eachMachedUserData of machedUserData) {
+            delete eachMachedUserData.userId;
+        }
+
+        array.push(machedUserData);
+    }
+    const newDatas = userIds.map((id, index) => {
+        return {
+            userId: id,
+            datas: array[index],
+        };
+    });
+    return newDatas;
+};
 
 module.exports = { userIdList, fixData };
